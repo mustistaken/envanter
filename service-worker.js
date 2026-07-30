8f753a37a4ab8a42e3ae21f033559b4b35d221da
TEKNİKEL <135001923+mustistaken@users.noreply.github.com>
2026-07-30 23:54:50 +0300

    Önbelleği yeni dosya yapısına geçir

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,13 @@
-const CACHE_NAME = 'teknikel-preview-v2';
-const APP_SHELL = ['./', './index.html', './manifest.json', './magmaweld-logo.png'];
+
+const CACHE_NAME = 'teknikel-preview-v3';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './styles.css?v=3',
+  './app.js?v=3',
+  './manifest.json',
+  './magmaweld-logo.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
