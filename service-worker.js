3f9cfa2dc2ae9140f67920a86a099b6ecfd98dfd
TEKNİKEL <135001923+mustistaken@users.noreply.github.com>
2026-07-31 03:49:42 +0300

    Önbelleği v14.6 için yenile

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'teknikel-v14-5';
+const CACHE_NAME = 'teknikel-v14-6';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=14.5',
-  './app.js?v=14.5',
+  './styles.css?v=14.6',
+  './app.js?v=14.6',
   './manifest.json',
   './magmaweld-logo.png',
   './icon.png'
