5796aa7da50a5a30fbce45559be34265a1eed175
TEKNİKEL <135001923+mustistaken@users.noreply.github.com>
2026-08-01 15:50:35 +0300

    Önbelleği v14.13 olarak yenile

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'teknikel-v14-12';
+const CACHE_NAME = 'teknikel-v14-13';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css?v=14.8',
-  './app.js?v=14.12',
+  './app.js?v=14.13',
   './manifest.json',
   './magmaweld-logo.png',
   './icon.png'
